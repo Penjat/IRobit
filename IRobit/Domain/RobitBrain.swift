81b596d7a01478ddb1f91080939a57ea9b7bc3a4
Penajt <theSpencerSymington@gmail.com>
2022-07-17 12:07:38 -0700

    Robit io.

--- IRobit/Domain/RobitBrain.swift
+++ IRobit/Domain/RobitBrain.swift
@@ -2,5 +2,6 @@ import Foundation
 import Combine
 
 class RobitBrain: ObservableObject {
-    
+    let commandInput = PassthroughSubject<RobitCommand, Never>()
+    @Published var movementOutput = RobitMovementOutput(motor1Speed: 0.0, motor2Speed: 0.0)
 }
---
+++ IRobit/Domain/RobitMovementOutput.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct RobitMovementOutput {
+    let motor1Speed: Double
+    let motor2Speed: Double
+}
